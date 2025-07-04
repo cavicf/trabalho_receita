f40d33befcc5d439ea5026f12c0f9bb39c70ee09
Camily <cavicfina@gmail.com>
2025-07-04 20:24:16 -0300

    feat:estilizacao da pagina de receita especifica

--- src/app/receitas/[id]/page.tsx
+++ src/app/receitas/[id]/page.tsx
@@ -7,7 +7,8 @@ import NotFound from '@/app/not-found';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Receita } from '@/lib/type';
-import { Heart } from 'lucide-react';
+import { Heart, ChevronLeft } from 'lucide-react';
+
 
 export default function ReceitaEspecificaPage() {
     const { id } = useParams();
@@ -29,35 +30,50 @@ export default function ReceitaEspecificaPage() {
         return NotFound();
     }
 
+    const ingredientes = receita.ingredientes.split(',');
+    const modoPreparo = receita.modo_preparo.split(/(?=\d+\.\s)/);
+
+
     return (
-    <main className="flex-grow py-8">
-        <div className="container mx-auto">
-            <div>
-                <Link href="/receitas">Voltar</Link>
-                <h1>{receita.receita}</h1>
+    <main className="flex-grow bg-[#F2EBE8] py-8">
+        <div className="flex flex-col gap-8 container bg-white py-8 px-12 rounded-3xl w-[50%] mx-auto">
+            <div className='lg:relative flex flex-col lg:flex-row items-center lg:justify-between lg:gap-0 gap-5'>
+                <Link href="/receitas" className='flex w-fit gap-1 items-center border border-[#995E4D] text-[#995E4D] px-3 active:scale-90 hover:scale-105 transition-transform py-1 rounded-md'>
+                    <ChevronLeft size={20}/>
+                    VOLTAR
+                </Link>
+                <h1 className='lg:absolute lg:left-1/2 lg:-translate-x-1/2 text-2xl lg:text-4xl text-[#F06E42]'>{receita.receita}</h1>
             </div>
-            <div>
-                <div className='relative aspect-[1/1] w-[50%]'>
+            <div className='flex flex-col gap-2'>
+                <div className='relative aspect-[16/10] w-full h-[20rem]'>
                     <Image 
                         src={receita.link_imagem} 
                         alt={receita.receita} 
                         fill 
-                        className="object-cover" 
+                        className="object-cover rounded-xl" 
                     />
                 </div>
-                {receita.tipo}
+                <h2 className="text-xl text-gray-800 bg-gray-200 px-1 rounded-sm w-fit">{receita.tipo}</h2>
             </div>
-            <div>
-                <h2>Ingredientes</h2>
-                {receita.ingredientes}
+            <div className='flex flex-col text-[#1C120D] gap-2'>
+                <h2 className='text-2xl font-semibold'>Ingredientes</h2>
+                <ul className='flex flex-col gap-1'>
+                    {ingredientes.map((ingrediente) => (
+                        <li key={ingrediente} className='flex intems-center gap-2'><input type="checkbox"/>{ingrediente}</li>
+                    ))}
+                </ul>
             </div>
-            <div>
-                <h2>Modo de Preparo</h2>
-                {receita.modo_preparo}
+            <div className='flex flex-col text-[#1C120D] gap-2'>
+                <h2 className='text-2xl font-semibold'>Modo de Preparo</h2>
+                <ul className='flex flex-col gap-1'>
+                    {modoPreparo.map((passo) => (
+                        <li key={passo} className='flex intems-center gap-2'>{passo}</li>
+                    ))}
+                </ul>
             </div>
-            <div>
-                <Link href={'/receitas'}>EXPLORAR MAIS</Link>
-                <button><Heart/>FAVORITAR</button>
+            <div className='flex gap-4 w-full'>
+                <Link href={'/receitas'} className='flex w-full justify-center items-center border border-[#995E4D] text-[#995E4D] px-3 active:scale-90 hover:scale-105 transition-transform py-1 rounded-xl'>EXPLORAR MAIS</Link>
+                <button className='flex justify-center items-center gap-2 bg-orange-500 w-full py-2 rounded-xl active:scale-95 hover:bg-orange-400 transition-colors'><Heart/>FAVORITAR</button>
             </div>
         </div>
     </main>
