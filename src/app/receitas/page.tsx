c01cfa24b7201a6d79ca182fcab81edd7b1d3da2
Camily <cavicfina@gmail.com>
2025-07-04 10:58:35 -0300

    feat: aficionando funcionalidade de pesquisar receitas

--- src/app/receitas/page.tsx
+++ src/app/receitas/page.tsx
@@ -20,27 +20,33 @@ export default function ReceitasPage() {
             }
         }
         fetchReceitas();
-    }, [])
+    }, []);
+
+    const [pesquisarReceita, setPesquisarReceita] = useState('');
+    const filtrarReceita = receitas.filter((receita) => (
+        receita.receita.toLowerCase().includes(pesquisarReceita.toLowerCase()) || 
+        receita.tipo.toLowerCase().includes(pesquisarReceita.toLowerCase())
+    ));
 
     return(
         <main className="flex-grow py-8 bg-white">
-            <div className='container mx-auto w-235 flex flex-col gap-10'>
+            <div className='container mx-auto w-full lg:w-235 flex flex-col gap-10'>
                 <section className='flex flex-col gap-6'>
                     <div className='flex flex-col items-center gap-2'>
                         <h1 className='text-4xl text-orange-500'>Explorar receitas</h1>
-                        <p className='w-[50%] text-2xl text-yellow-950'>
+                        <p className='w-[50%] text-2xl text-center text-yellow-950'>
                             Aqui você vê as receitas das pessoas
                             que também adoram o <span className='text-red-500'>universo gastronômico!</span>
                         </p>
                     </div>
-                    <div className='relative w-ful'>
+                    <div className='relative w-full'>
                         <Search className='absolute left-4 top-1/2 transform -translate-y-1/2' size={18} color={'#A66541'}/>
-                        <input type="text" placeholder='Pesquisar receita' className='w-full py-3 px-11 bg-[#F2EBE8] rounded-md  placeholder-[#A66541]'/>
+                        <input type="text" placeholder='Pesquisar receita' className='w-full text-[#A66541] py-3 px-11 bg-[#F2EBE8] rounded-md  placeholder-[#A66541]' value={pesquisarReceita} onChange={(e: React.ChangeEvent<HTMLInputElement>) => (setPesquisarReceita(e.target.value))}/>
                     </div>
                 </section>
                 <section className='rounded-3xl w-full bg-[url("/background/background.png")] bg-cover bg-center p-3'>
-                    <div className='flex flex-wrap gap-6 justify-center'>
-                        {receitas.map((receita, index) => (
+                    <div className='flex flex-wrap gap-6'>
+                        {filtrarReceita.map((receita, index) => (
                             <ReceitaCard key={index} receita={receita}/>
                         ))}
                     </div>
